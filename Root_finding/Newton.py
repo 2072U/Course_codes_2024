17637cc67a4499e371cb85a0ffd6e0bc6208998e
lvanveen <lennaert.vanveen@ontariotechu.ca>
2024-01-25 11:23:15 -0500

    Added comments to the bisection and Newton functions.

--- Root_finding/Newton.py
+++ Root_finding/Newton.py
@@ -1,21 +1,23 @@
+# Newton iteration for finding the root of a function. L. van Veen, Ontario Tech U, 2024.
 import numpy as np
 
+# Inputs: function handles f and fp (derivative of f), initial guess x0, max nr. of iterations kMax, tolrance for error (epsX) and residual (epsF).
 def Newton(f,fp,x0,kMax,epsX,epsF):
-    record = np.zeros((kMax,3))
-    x = x0
+    record = np.zeros((kMax,3))                    # This serves only to plot the errors/residuals later.
+    x = x0                                         # Convergence flag set to "no convergence" by default.
     conv = 0
-    for i in range(kMax):
-        r = f(x)
-        df = fp(x)
-        dx = -r/df
-        err = abs(dx)
+    for i in range(kMax):                          # Loop over iterations.
+        r = f(x)                                   # Evaluate f...
+        df = fp(x)                                 # ... and its derivative.
+        dx = -r/df                                 # Compute the Newton update step.
+        err = abs(dx)                              # Compute an estimate of the error and the residual.
         res = abs(r)
         record[i,:] = [i,err,res]
         print('Iteration %d, err=%e, res=%e' % (i,err,res))
-        if err < epsX and res < epsF:
+        if err < epsX and res < epsF:              # Check for comvergence.
             conv = 1
             break
-        x += dx
+        x += dx                                    # Update the approximate solution.
     if conv == 0:
         print('No convergence!')
-    return x,record
+    return x,record                                # You may want to return the convergence flag, too!
--- Root_finding/bisect.py
+++ Root_finding/bisect.py
@@ -1,26 +1,28 @@
+# Bisection for finding the root of a function. L. van Veen, Ontario Tech U, 2024.
 import numpy as np
 
+# Inputs: function handle f, initial domain (a,b), max nr. of iterations kMax, tolrance for error (epsX) and residual (epsF).
 def bisect(f, a, b, kMax, epsX, epsF):
-    record = np.zeros((kMax,3))
-    conv = 0
+    record = np.zeros((kMax,3))                    # This serves only to plot the errors/residuals later.
+    conv = 0                                       # Convergence flag set to "no convergence" by default.
     l = a
     r = b
-    for i in range(0,kMax):
-        m = (l+r)/2
-        fm = f(m)
+    for i in range(0,kMax):                        # Loop iver iterations.
+        m = (l+r)/2                                # Find the mid point.
+        fm = f(m)                                  # Compute the function value there and at the left boundary.
         fl = f(l)
-        err = abs(r-l)
+        err = abs(r-l)                             # Compute the error and residual.
         res = abs(f(m))
         record[i,:] = [i,err,res]
         print('It %d, x=%f, err=%e' % (i,m,err))
-        if err < epsX and res < epsF:
+        if err < epsX and res < epsF:              # Test for convergence.
             conv = 1
             break
-        if fl * fm > 0:
+        if fl * fm > 0:                            # Update the domain.
             l = m
         else:
             r = m
 
-    if conv == 0:
+    if conv == 0:                                  # Print a warning if the there was no convergence.
         print('No convergence in bisection.')
-    return m,record
+    return m,record                                # You may want to return the convergence flag, too!
